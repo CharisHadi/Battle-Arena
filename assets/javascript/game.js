1a199330f759e1c67df9951c8ed9c2c0f38cce1f
CharisHadi <charishadi@gmail.com>
2019-05-04 18:48:38 -0700

    Change Card Generation
    -Dynamically generate cards (opposed to manual creation in HTML)
    -attack button goes after the cards

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,43 +6,60 @@ attack: the amount the character attacks for (friendly heroes only)
 counter: the amount a character will retaliate for (enemy heroes only)
 */
 var characters = {
-    char1 : {
-        name : "guy one",
+    char0 : {
+        name : "Captain",
         health : 100,
         base: 20,
         attack : 20,
         counter : 15,
+        img: "assets/images/Captain.png",
     },
 
-    char2 : {
-        name : "guy two",
+    char1 : {
+        name : "Commando",
         health : 180,
         base: 8,
         attack : 8,
         counter : 30,
+        img: "assets/images/Commando.png",
     },
 
-    char3 : {
-        name : "guy three",
+    char2 : {
+        name : "Huntress",
         health : 60,
         base : 30,
         attack : 30,
         counter : 10,
+        img: "assets/images/Huntress.png",
     },
 
-    char4 : {
-        name : "guy four",
+    char3 : {
+        name : "Engineer",
         health : 120,
         base : 10,
         attack : 10,
         counter : 20,
+        img: "assets/images/Engineer.png",
     }
 }
 
-var char_list = [char1, char2, char3, char4];
+var char_list = [characters.char0, characters.char1, characters.char2, characters.char3];
+
+
+//Populate html dynamically with hero cards
+for (char_index = 0; char_index < char_list.length; char_index++){
+    console.log(char_index);
+    $('#character-select').append('<div id = "card-' + char_index + '" class = "col-md-2">');
+    $('#card-' + char_index + '.col-md-2').append('<div id = "' + char_list[char_index].name + '" class = "herocard neutral">');
+    $('#' + char_list[char_index].name + '.herocard').append('<h6>' + char_list[char_index].name + '</h6>');
+    $('#' + char_list[char_index].name + '.herocard').append('<img id= "' + 'char' + char_index + '" class= "portrait" ' + 'src = ' + char_list[char_index].img + '>');
+
+}
+
+$('#character-select').append('<button type= "button" class= "btn btn-danger" onclick = "attack(characters.char1, characters.char4)">Attack</button>')
 
 //function for actions relating to clicking on character, mainly selection/delegation
-$(".portrait").click(function (e){
+$('.portrait').click(function (e){
     //Checks if heroes are neutral and delegates them to proper team
     if (e.target.parentElement.classList.contains("neutral")){
         console.log(characters[e.target.id]);
